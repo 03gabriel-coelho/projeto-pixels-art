56d8b8a76f4ead9c6578e208b833de0b5e718030
Gabriel Coelho <coelho.tibbers@gmail.com>
2021-12-10 18:42:10 -0300

    Requisito 7 ok

--- script.js
+++ script.js
@@ -10,7 +10,58 @@ adicionar.className = 'pixel';
 }
 addPixels();
 
-let color1 = document.getElementById('color1');
+let color1 = document.getElementById('color1');    //Cor Inicial
 color1.classList.add('selected');
 
-console.log(color1);
+let color2 = document.getElementById('color2');
+let color3 = document.getElementById('color3');
+let color4 = document.getElementById('color4');
+
+function selectedColor1 (){
+    for(let index = 0; index < color1.classList.length; index += 1){
+        if (color1.classList[index] !== 'selected'){
+            color1.classList.add('selected');
+            color2.classList.remove('selected');
+            color3.classList.remove('selected');
+            color4.classList.remove('selected');
+        }
+    }
+}
+color1.addEventListener('click', selectedColor1);
+
+function selectedColor2 (){
+    for(let index = 0; index < color2.classList.length; index += 1){
+        if (color2.classList[index] !== 'selected'){
+            color2.classList.add('selected');
+            color1.classList.remove('selected');
+            color3.classList.remove('selected');
+            color4.classList.remove('selected');
+        }
+    }
+}
+color2.addEventListener('click',selectedColor2);
+
+function selectedColor3 (){
+    for(let index = 0; index < color3.classList.length; index += 1){
+        if (color3.classList[index] !== 'selected'){
+            color3.classList.add('selected');
+            color1.classList.remove('selected');
+            color2.classList.remove('selected');
+            color4.classList.remove('selected');
+        }
+    }
+}
+color3.addEventListener('click',selectedColor3);
+
+function selectedColor4 (){
+    for(let index = 0; index < color4.classList.length; index += 1){
+        if (color4.classList[index] !== 'selected'){
+            color4.classList.add('selected');
+            color1.classList.remove('selected');
+            color3.classList.remove('selected');
+            color2.classList.remove('selected');
+        }
+    }
+}
+color4.addEventListener('click',selectedColor4);
+console.log(color1.classList.length);
